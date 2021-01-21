77afcc77a6999de9cfcd25b43bbca85d79fab41d
Agnes Kabongo <57146479+agkabongo@users.noreply.github.com>
2021-01-21 19:48:15 +0100

    deadlink

--- pages/profile.php
+++ pages/profile.php
@@ -5,7 +5,7 @@ include('../components/functions.php');
 profile();
 
 if($_SESSION['account_type'] == 'ADMIN'){
-    header("location: /profileadmin.php");
+    header("location: public/breaking_badge/pages/profileadmin.php");
 }
 ?>
 
